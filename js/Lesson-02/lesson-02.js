5022741c42147f40b7afc0ba3b1279c03dc61cc0
Anchik17 <151466304+Anchik17@users.noreply.github.com>
2024-04-07 14:36:22 +0200

    Update lesson-02.js

--- js/Lesson-02/lesson-02.js
+++ js/Lesson-02/lesson-02.js
@@ -5,21 +5,68 @@
 // Замініть значення "Блюз" на "Класика".
 // Видалить перший елемент масиву і виведіть його в консоль.
 // Вставте «Реп» і «Реггі» на початок масиву.
-const styles = ['Jazz', 'Bluz'];
-styles.push('Rock-Roll');
-styles.unshift('Tehno');
-// styles[1] = 'Classic';
-const index = styles.indexOf('Bluz');
-styles[index] = 'Classic';
-const indx = styles.indexOf('Classic');
-if (indx !== -1) {
-  styles.splice(indx, 1);
-}
+// const styles = ['Jazz', 'Bluz'];
+// styles.push('Rock-Roll');
+// styles.unshift('Tehno');
+// // styles[1] = 'Classic';
+// const index = styles.indexOf('Bluz');
+// styles[index] = 'Classic';
+// const indx = styles.indexOf('Classic');
+// if (indx !== -1) {
+//   styles.splice(indx, 1);
+// }
 
-console.log(styles.join(' '));
-console.log(styles.shift());
-console.log(styles);
+// console.log(styles.join(' '));
+// console.log(styles.shift());
+// console.log(styles);
 
-for (const style of styles) {
-  console.log(style);
-}
+// for (const style of styles) {
+//   console.log(style);
+// }
+
+
+
+//TODO:============task-05==============
+// Напишіть функцію min(a, b), яка повертає
+// менше з чисел a, b
+// Потрібно додати перевірку, що функція отримує числа
+
+
+// function min(a = 10, b = 5) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return "Not a number";
+//   }
+//   return Math.min(a,b);
+// }
+
+
+// console.log(min(9, 10));
+
+
+
+
+
+//TODO:=========task-06=================
+//Напишіть функцію calculateAverage()
+//яка приймає довільну кількість
+//аргументів і повертає їхнє середнє значення.
+//Додати перевірку, що аргументи це числа.
+
+
+// function calculateAverage() {
+//   let total = 0;
+//   let count = 0;
+
+//   for (const arg of arguments) {
+//     if (typeof arg !== "number") {
+//       continue;
+//     }
+//     total += arg;
+//     count += 1;
+//   }
+//   return Math.floor(total / count);
+// }
+
+
+// console.log(calculateAverage(5, 5, "true"));
+// console.log(calculateAverage(5, 65, 44, 23, 2));
